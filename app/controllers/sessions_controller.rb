d5749e8c85cdc4a0afc8225d332a782ee874c5a1
sangfor <huct2009@gmail.com>
2013-09-01 19:34:54 +0800

    paginate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 		#Sign in ok
 		 sign_in user
-         redirect_to user
+         redirect_back_or user
 		else
 		#Error
 		 flash.now[:error] = 'Invalid email/password combination' # Not quite right!
